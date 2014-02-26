19c705cdcc7f6849bd9db7f35aa9efc754992818
Kyle Adams <kyle@geek.net>
2014-02-26 18:46:56 -0500

    Added example/test server.

---
+++ example/server.py
@@ -0,0 +1,88 @@
+import pkg_resources
+from decorator import decorator
+
+import bobo
+from mako.template import Template
+from bson import json_util
+
+from switchboard import operator, configure
+from switchboard.admin.controllers import CoreAdminController
+
+configure()
+
+
+@decorator
+def safejson(f, *args, **kw):
+    '''
+    Needed since Bobo's built-in JSON marshalling doesn't handle datetime
+    or ObjectID fields.
+    '''
+    return json_util.dumps(f(*args, **kw))
+
+
+@bobo.query('/')
+def index():
+    if operator.is_active('example'):
+        return 'The example switch is active.'
+    else:
+        return 'The example switch is NOT active.'
+
+
+@bobo.subroute
+def admin(request):
+    return AdminController()
+
+
+@bobo.scan_class
+class AdminController:
+    def __init__(self):
+        self.c = CoreAdminController()
+
+    @bobo.resource('')
+    def base(self, bobo_request):
+        return bobo.redirect(bobo_request.url + '/')
+
+    @bobo.query('/')
+    def index(self, by='-date_modified'):
+        path = pkg_resources.resource_filename('switchboard.admin.templates',
+                                               'index.mak')
+        with open(path, 'r') as f:
+            tmpl = f.read()
+        data = self.c.index(by)
+        html = Template(tmpl).render(**data)
+        return html
+
+    @bobo.post('/add', content_type='application/json')
+    @safejson
+    def add(self, key, label='', description=None, **kwargs):
+        return self.c.add(key, label, description, **kwargs)
+
+    @bobo.post('/update', content_type='application/json')
+    @safejson
+    def update(self, curkey, key, label='', description=None):
+        return self.c.update(curkey, key, label, description)
+
+    @bobo.post('/status', content_type='application/json')
+    @safejson
+    def status(self, key, status):
+        return self.c.status(key, status)
+
+    @bobo.post('/delete', content_type='application/json')
+    @safejson
+    def delete(self, key):
+        return self.c.delete(key)
+
+    @bobo.post('/add_condition', content_type='application/json')
+    @safejson
+    def add_condition(self, bobo_request):
+        return self.c.add_condition(**bobo_request.POST)
+
+    @bobo.post('/remove_condition', content_type='application/json')
+    @safejson
+    def remove_condition(self, bobo_request):
+        return self.c.remove_condition(**bobo_request.POST)
+
+    @bobo.query('/history', content_type='application/json')
+    @safejson
+    def history(self, key):
+        return self.c.history(key)
