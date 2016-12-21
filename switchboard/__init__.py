4a5fa374f766c87a017228a71b4bfc4cc3ac011f
Kyle Adams <kadams54@gmail.com>
2016-12-21 10:56:25 -0500

    Coverage on conditions.

--- switchboard/__init__.py
+++ switchboard/__init__.py
@@ -6,12 +6,12 @@ switchboard
 :license: Apache License 2.0, see LICENSE for more details.
 """
 
+from .manager import operator, configure
+
 __all__ = ('operator', 'configure', 'VERSION')
 
 try:
     VERSION = __import__('pkg_resources') \
         .get_distribution('switchboard').version
-except Exception, e:
+except Exception, e:  # pragma: nocover
     VERSION = 'unknown'
-
-from .manager import operator, configure
