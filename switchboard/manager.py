b5614880332e5c61abd8156b16850146705ab575
Dave Brondsema <dave@brondsema.net>
2019-07-10 17:23:03 -0400

    Fix failing tests, run tests with tox

--- switchboard/manager.py
+++ switchboard/manager.py
@@ -94,7 +94,7 @@ class SwitchManager(ModelDict):
             dic = self.result_cache
             cache_key = None
             if dic is not None:
-                cache_key = (args, tuple(kwargs.items()))
+                cache_key = (args, tuple(sorted(kwargs.items())))
                 try:
                     result = dic.get(cache_key)
                 except TypeError as e:  # not hashable
