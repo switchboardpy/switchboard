d7e04de310fc9e4c717f8ce3aa40a798c8006961
Dave Brondsema <dbrondsema@slashdotmedia.com>
2022-03-16 16:56:52 -0400

    Drop old python and pymongo versions

--- setup.py
+++ setup.py
@@ -1,28 +1,16 @@
 from __future__ import unicode_literals
 from __future__ import absolute_import
 import setuptools
-import sys
 
 VERSION = '1.5.3'
 INSTALL_REQUIRES = [
-    'pymongo >= 2.4',
+    'pymongo >= 3',
     'blinker >= 1.2',
     'WebOb >= 0.9',
     'Mako >= 0.9',
     'bottle >= 0.12.8',
     'six',
 ]
-EXTRAS_REQUIRE = {}
-
-# https://hynek.me/articles/conditional-python-dependencies/
-# Add a conditional dependency on the ipaddress library for validating IP
-# addresses; only needed for Python < 3.3.
-if int(setuptools.__version__.split('.', 1)[0]) < 18:
-    assert 'bdist_wheel' not in sys.argv
-    if sys.version_info[0:2] < (3, 3):
-        INSTALL_REQUIRES.append('ipaddress')
-else:
-    EXTRAS_REQUIRE[":python_version<'3.3'"] = ['ipaddress']
 
 setuptools.setup(
     name='switchboard',
@@ -31,8 +19,11 @@ setuptools.setup(
     # http://www.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
         "Programming Language :: Python",
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
+    python_requires='>=3.7',
     keywords='switches feature flipper pyramid pylons turbogears',
     author='Kyle Adams',
     author_email='kadams54@users.noreply.github.com',
@@ -42,7 +33,6 @@ setuptools.setup(
     packages=setuptools.find_packages(exclude=['ez_setup']),
     include_package_data=True,
     install_requires=INSTALL_REQUIRES,
-    extras_require=EXTRAS_REQUIRE,
     zip_safe=False,
     tests_require=[
         'nose',
