c3a394b9101adbed4e1b4405ab09111ae7b5b19c
Dave Brondsema <dave@brondsema.net>
2024-05-13 11:49:50 -0400

    replace pkg_resources with importlib.metadata

--- switchboard/__init__.py
+++ switchboard/__init__.py
@@ -5,13 +5,13 @@ switchboard
 :copyright: (c) 2015 Kyle Adams.
 :license: Apache License 2.0, see LICENSE for more details.
 """
+from importlib.metadata import version
 
 from .manager import operator, configure
 
 __all__ = ('operator', 'configure', 'VERSION')
 
 try:
-    VERSION = __import__('pkg_resources') \
-        .get_distribution('switchboard').version
+    VERSION = version('switchboard')
 except Exception as e:  # pragma: nocover
     VERSION = 'unknown'
