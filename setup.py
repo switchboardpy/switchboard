10cded10787ff46e3beb7b553cdb9f531270a883
Dave Brondsema <dave@brondsema.net>
2025-08-18 18:05:54 -0400

    mark support for python 3.14

--- setup.py
+++ setup.py
@@ -28,6 +28,7 @@ setuptools.setup(
         'Programming Language :: Python :: 3.11',
         'Programming Language :: Python :: 3.12',
         'Programming Language :: Python :: 3.13',
+        'Programming Language :: Python :: 3.14',
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
     python_requires='>=3.9',
